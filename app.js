62b8273d1ddd700ff4902781387efad79023871c
John Sciutto <johnsciutto@gmail.com>
2020-10-26 16:59:06 -0400

    Organized file

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
+const DB_ADDRESS = 'mongodb://localhost:27017/wikiDB';
+const PORT = process.env.PORT || 3000;
+
+// Express Configuration =======================================================
+
 const app = express();
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// Mongoose Configuration ======================================================
+
+mongoose.connect(DB_ADDRESS, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const articleSchema = new mongoose.Schema({
   title: String,
@@ -16,8 +26,10 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model('Article', articleSchema);
 
+// Routes ======================================================================
+
 app.get('/', (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
